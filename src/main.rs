0c5c22a436e63af084b9a81716fb3f9949a85413
Daiki48 <daiki48.engineer@gmail.com>
2022-10-15 18:17:47 +0900

    [update] modularization

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,6 @@
-mod todo_input;
+mod todo_model;
 
-use todo_input::{
-    get_title,
-    get_contents
-};
-
-#[derive(Debug)]
-struct Todo {
-    title: String,
-    contents: String,
-    status: bool,
-}
-
-impl Todo {
-    fn new() -> Self {
-        Todo {
-            title: "".to_string(),
-            contents: "".to_string(),
-            status: true,
-        }
-    }
-
-    fn insert(&mut self) {
-        println!("Enter the title of the TODO.");
-        let todo_title = get_title();
-        self.title.push_str(&todo_title);
-        println!("{}", &mut self.title);
-
-        println!("Enter the contents of the TODO.");
-        let todo_contents = get_contents();
-        self.contents.push_str(&todo_contents);
-        println!("{}", &mut self.contents);
-    }
-}
+use todo_model::Todo;
 
 fn main() {
     std::process::Command::new("clear").status().unwrap();
